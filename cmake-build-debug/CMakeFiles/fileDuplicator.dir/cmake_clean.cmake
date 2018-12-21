09afe9dcb4154280986ed555bfd98e710fe51b05
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2018-12-21 01:56:07 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/fileDuplicator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileDuplicator.dir/main.c.o"
+  "fileDuplicator.pdb"
+  "fileDuplicator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fileDuplicator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
